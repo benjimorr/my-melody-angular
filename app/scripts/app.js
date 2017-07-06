a81d5b334d5898ade762a493d4cc3c880c7b779f
Ben Morrison <benmorrison0@gmail.com>
2017-07-06 00:24:20 -0400

    Checkpoint 3 routing & states: completed

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,2 +1,26 @@
-// define the blocJams module with angular.module:
-angular.module('blocJams', []);
+// config function:
+function config($stateProvider, $locationProvider) {
+    $locationProvider
+        .html5Mode({
+            enabled: true,
+            requireBase: false
+        });
+
+    $stateProvider
+        .state('landing', {
+            url: '/',
+            templateUrl: '/templates/landing.html'
+        })
+        .state('album', {
+            url: '/album',
+            templateUrl: '/templates/album.html'
+        })
+        // collection state here
+}
+
+//defining the angular module:
+(function() {
+    angular
+        .module('blocJams', ['ui.router'])
+        .config(config, 'config');
+})();
