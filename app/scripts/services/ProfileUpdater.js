3b341e8ad824d1947b8c6485becb9cbdaa900a4d
Ben Morrison <benmorrison0@gmail.com>
2017-07-18 15:03:42 -0400

    Profile template finalized

--- app/scripts/services/ProfileUpdater.js
+++ app/scripts/services/ProfileUpdater.js
@@ -2,7 +2,43 @@
     function ProfileUpdater($cookies) {
         var ProfileUpdater = {};
 
+        /*
+        * @desc The first name of the user, stored in cookies
+        * @type {String}
+        */
+        ProfileUpdater.firstName = $cookies.get('firstName') || "";
 
+        /*
+        * @desc The last name of the user, stored in cookies
+        * @type {String}
+        */
+        ProfileUpdater.lastName = $cookies.get('lastName') || "";
+
+        /*
+        * @desc The email address of the user, stored in cookies
+        * @type {String}
+        */
+        ProfileUpdater.email = $cookies.get('email') || "";
+
+        /*
+        * @desc The hometown of the user, stored in cookies
+        * @type {String}
+        */
+        ProfileUpdater.hometown = $cookies.get('hometown') || "";
+
+        /*
+        * @desc The full name of the user, stored in cookies
+        * @type {String}
+        */
+        ProfileUpdater.fullName = $cookies.get('fullName') || "Edit Profile";
+
+        /*
+        * @function saveProfile
+        * @desc Saves the user's profile data to local cookies
+        */
+        ProfileUpdater.saveProfile = function() {
+            //functionality here
+        };
 
         return ProfileUpdater;
     }
